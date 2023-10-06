d7d048130aa6966f8f273ccc90e7ddbb4284e644
priyank rai <type2email@gmail.com>
2023-10-06 18:47:47 +0530

    feat: added client side protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,21 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
   try {
-    const { pathName } = request.nextUrl;
-    const isPublicPath = pathName === "/login" || pathName === "/signup";
-    const token = request.cookies.get("token")?.value || "";
+    const { pathname } = request.nextUrl;
+    const isPublicPath = pathname === "/login" || pathname === "/signup";
+    const token = request.cookies.get("token")?.value || 0;
 
-    if(!isPublicPath && !token)
-    {
-         
+    if (isPublicPath && token) {
+      return NextResponse.redirect(new URL("/profile", request.nextUrl));
     }
-
+    if (!isPublicPath && !token) {
+      return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
   } catch (error: any) {
     throw new Error(error.message);
   }
 }
 
 export const config = {
-  matcher: ["/", "/profile/(.*)", "/login", "/signup"],
+  matcher: ["/", "/profile/(.*)", "/login", "/signup", "/profile"],
 };
